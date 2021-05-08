d662734de22d3451b5ecfce38e5de08a4f92c4c9
maqsoodahamed458 <83702598+maqsoodahamed458@users.noreply.github.com>
2021-05-08 13:09:36 +0530

    added Day3 class

---
+++ Day3_B44.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continue with string 
+
+
+# In[1]:
+
+
+full_name='maqsood'
+print(full_name)
+
+
+# In[2]:
+
+
+print(full_name.title())
+
+
+# In[3]:
+
+
+print(full_name.upper())
+
+
+# In[4]:
+
+
+print(full_name.lower())
+
+
+# In[ ]:
+
+
+introduction to f string/     f"customerword{placeholder}{placeholder}...{placeholder}"
+
+
+# In[7]:
+
+
+firstname='maqsood'
+lastname='ahamed'
+fullname=f"{firstname} {lastname}"
+
+
+# In[8]:
+
+
+print(fullname)
+
+
+# In[9]:
+
+
+print(fullname.title())
+
+
+# In[11]:
+
+
+print(f"hai,{fullname.title()}")
+
+
+# In[12]:
+
+
+print(f"how are you,{fullname.title()}")
+
+
+# In[16]:
+
+
+print("my favourite programming language:c c++ java python javascript")
+
+
+# In[17]:
+
+
+print("my favourite programming language:c \nc++\n java\n python\n  javascript")
+
+
+# In[26]:
+
+
+my_fav_progm_lang="python"
+print(my_fav_progm_lang)
+
+
+# In[30]:
+
+
+my_fav_progm_lang="        python"
+print(my_fav_progm_lang)
+
+
+# In[27]:
+
+
+my_fav_progm_lang="        python"
+print(my_fav_progm_lang.lstrip())
+
+
+# In[32]:
+
+
+my_fav_progm_lang="        python       "
+print(my_fav_progm_lang)
+
+
+# In[28]:
+
+
+my_fav_progm_lang="        python       "
+print(my_fav_progm_lang.strip())
+
+
+# In[29]:
+
+
+my_fav_progm_lang="python       "
+print(my_fav_progm_lang.rstrip())
+
+
+# In[ ]:
+
+
+
+
